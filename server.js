f33affd24b3a91f5802e77c0a8d66fdc38d1bd9d
Paul T <paultoro101@gmail.com>
2021-01-27 19:03:59 -0500

    added npm files

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoute");
+const apiRoutes = require("./routes/apiRoute");
 
-var express = require("express");
-var path = require('path')
+const app = express();
+const PORT = process.env.PORT || 8081;
 
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT:" + PORT);
-});
+app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`));
